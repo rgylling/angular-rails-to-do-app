5b1d3c3fb96792d144e69e5a48544d129e23ec8b
rgylling <mikegylling@yahoo.com>
2016-12-21 16:50:46 -0800

    making sure json rendering works

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,7 +1,19 @@
 class TodoListsController < ApplicationController
 
+  before_action :set_list, only: [ :show]
+
   def index
-    
+
   end
 
+  def show
+    render json: @list
+  end
+
+
+  private
+
+  def set_list
+    @list = TodoList.find_by_id(params[:id])
+  end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
-class Todo
+class Todo < ApplicationRecord
   belongs_to :todo_list
 
 end
