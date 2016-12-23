f5125f3a1ef5a7661be7c02ccbde97a30154f5f0
rgylling <mikegylling@yahoo.com>
2016-12-22 16:09:06 -0800

    attempting to get a quick api response

--- app/assets/javascripts/controllers/ListController.js
+++ app/assets/javascripts/controllers/ListController.js
@@ -4,8 +4,15 @@
 
     angular
         .module('todoApp')
-        .controller('ListController', function() {
+        .controller('ListController',['ListFactory' function(ListFactory) {
             var vm = this
             vm.test = "Testing"
-        })
+
+            ListFactory.getLists()
+                       .then(setLists)
+            function setLists(data) {
+              vm.lists = data
+              console.log(data)
+            }
+        }])
 }())
