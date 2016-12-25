b8f88fad6b10c77cdf5f1d1b9aa0af8f1b118a70
rgylling <mikegylling@yahoo.com>
2016-12-25 13:20:26 -0800

    working on list controller

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,6 +1,6 @@
 class TodoListsController < ApplicationController
 
-  before_action :set_list, only: [ :show]
+  before_action :set_list, only: [ :show, :update, :destroy]
 
   def index
     render json: TodoList.all
@@ -10,8 +10,22 @@ class TodoListsController < ApplicationController
     render json: @list
   end
 
+  def update
+    if @list.update(list_params)
+    end
+
+  end
+
+  def destroy
+    @list.destroy
+  end
+
 
   private
+
+  def list_params
+    params.require(:todo).permit(:name)
+  end
 
   def set_list
     @list = TodoList.find_by_id(params[:id])
